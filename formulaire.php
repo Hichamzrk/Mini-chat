7acfbb654b4b04a4ff374c3841e3c42f51c91d3c
“Hichamzrk” <hichamzarrouk11047@gmail.com>
2020-06-20 19:24:55 +0200

    V1

--- formulaire.php
+++ formulaire.php
@@ -1,29 +1,25 @@
 <?php  
-//Connexion à la base de donnée 
-  $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8' ,'root',  'root');
+	session_start();
 ?>
 
 <?php
-//affichage des erreurs
-try
-{
+
+//connexion à la base de donné
+
   $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
-}
-catch (Exception $e)
-{
-        die('Erreur : ' . $e->getMessage());
-}
+
 ?>
 
 
 <?php
 //requete préparer stockant les pseudos et messanges dans la base de donnée  
-  $req = $bdd->prepare('INSERT INTO Message (pseudo, message) VALUES(?, ?)');
+  $req = $bdd->prepare('INSERT INTO Message (pseudo, message, avatar) VALUES(?, ?, ?)');
   $req->execute(array(
-      $_POST['pseudo'],
-      $_POST['message']
+      $_SESSION['pseudo'],
+      $_POST['message'],
+      $_SESSION['avatar']
   ));
 
 
-header('location: index.php');
+header('location: chat.php');
 ?>
--- index.php
+++ index.php
@@ -1,64 +1,27 @@
-
 <?php 
-//Connexion à la base de donnée 
-	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8' ,'root',  'root');
-?>
+	session_start()
+ ?>
 
-<?php
-//affichage des erreurs
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
-}
-catch (Exception $e)
-{
-        die('Erreur : ' . $e->getMessage());
-}
-?>
-
-<?php
-$message = $bdd->query('SELECT * FROM Message ORDER BY ID DESC LIMIT 0, 5')
-?>
-
-
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
-   <head>
-       <title>Mini chat</title>
-       <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-       <link rel="stylesheet" type="text/css" href="chat.css">
-   </head>
-
-   <body>
-		<article>
-			
-			<div>
-				<?php 
-				//Affichage des messages et des pseudo stocké dans la base donnée
-						while ($donnees = $message->fetch()) {
-					 ?>
-
-					 	<p>
-					 		<img src="./image/avatar.png">
-					 		<strong><?php echo htmlspecialchars($donnees['pseudo']);?></strong>  <br>
-					 		<?php echo htmlspecialchars($donnees['message']);?> <br>
-							<em><?php echo htmlspecialchars($donnees['heure']);?></em> 
-					 	</p>
-					<?php
-					}
-				?>
-			</div>
-		  
-		   	<form method="post" action="formulaire.php">
-	
-			   	 <input type="text" placeholder="pseudo" name="pseudo"required="required"> 
-			   	 <textarea style="resize: none;" name="message" required placeholder="message"> </textarea> 
-			   	<input type="submit" name="envoyer" value="Go">
-
-			</form>
-			
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Connexion</title>
+	<style type="connexion.css"></style>
+</head>
+	<body>
+		</html>
 		
-		</article>
-   </body>
+		 <form action="session.php" method="post">
+		 	<input type="text" name="pseudo">
+		 	<label for="avatar">
+			 	<input type="radio" name="avatar" id="avatar">
+			 	<img src="/image/avatar.png">
+			</label>
+			<label for="avatar2">
+		 		<input type="radio" name="avatar" id="avatar2">
+			 	<img src="/image/avatar2.png">
+			</label>
+		 	<input type="submit" name="envoyer">
+		 </form>
+	</body>
 </html>
